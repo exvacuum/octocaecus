59df0ea7db3b390a52608b59f307844cab47d6a5
Si Bartha <silas@exvacuum.dev>
2022-07-24 16:42:07 -0400

    Added comments lol

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use std::io;
 use std::env;
 
+// Minimum and maximum braille code points
 const CAECUS_MIN:u16 = 0x2800;
 const CAECUS_MAX:u16 = 0x28FF;
+
+// Position of each incoming bit on the braille character
 const CAECUS_POS_ENCODE:[u8;8] = [0,1,2,6,3,4,5,7];
+
+// Position of each bit when decoding a braille character
 const CAECUS_POS_DECODE:[u8;8] = [0,1,2,4,5,6,3,7];
 
+// Encodes an 8-bit value to its corresponding braille character
 fn caecus_char(octo:u8) -> char {
+
+    // Rearrange necessary bits
     let mut rearranged = 0u8;
     for i in 0..8 {
         if octo & (1 << i) == (1 << i) {
@@ -26,9 +34,11 @@ fn caecus_char(octo:u8) -> char {
     }
 }
 
+// Decodes a braille character to its corresponding 8-bit value
 fn caecus_int(c:char) -> u8 {
     let number = c as u16 - CAECUS_MIN;
     
+    // Rearrange neccessary bits
     let mut rearranged = 0u8;
     for i in 0..8 {
         if number & (1 << i) == (1 << i) {
@@ -40,13 +50,19 @@ fn caecus_int(c:char) -> u8 {
 }
 
 fn main() {
+   
+    // If "-d" argument is passed, use decode mode
     let args: Vec<String> = env::args().collect();
-    let decode = args.len() > 1 && &args[1] == "-d"; 
+    let decode = args.len() > 1 && &args[1] == "-d";
+
+    // Read data from stdin
     for line in io::stdin().lines() {
         match line {
             Ok(mut line) => { 
                 if decode {
+                    // Strip all whitespace from line
                     line.retain(|c| !c.is_whitespace());
+                    // Process input as octal triples
                     for i in (0..line.len()).step_by(3) {
                         print!("{}", caecus_char(match u8::from_str_radix(&line[i..i+3], 8) {
                             Ok(octo) => octo,
@@ -57,6 +73,7 @@ fn main() {
                     continue;
                 }
 
+                // Decode each braille character
                 for c in line.chars() {
                     print!(" {:03o}", caecus_int(c));
                 }
